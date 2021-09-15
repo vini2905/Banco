0820ac77abb147a77acaef7ee4577e6e2c7c101e
vinicius <viniluzpere2017@gmail.com>
2021-09-15 14:48:00 -0300

    adicionei os arquivos agenda carrinho infox

--- infox.sql
+++ infox.sql
@@ -48,3 +48,49 @@ values ('gustavo','guh@usuario',md5('11223'));
 
 insert into usuarios (usuario, login, senha)
 values ('dylan','dylan@usuario',md5('66666'));
+
+
+
+-- Tabela de clientes (clientes da assistência técnica)
+create table clientes(
+idcli int primary key auto_increment,
+nome varchar(50) not null,
+fone varchar(15) not null
+);
+
+-- mostrar a tabela
+describe clientes;
+
+-- tabela de OS (ordem de serviços)
+create table tbos(
+  os int primary key auto_increment,
+ equipamento varchar(250) not null,
+ defeito varchar(250) not null,
+ dataos timestamp default current_timestamp,
+    statusOs varchar(50) not null,
+    valor decimal(10,2),
+    idcli int not null,
+    foreign key(idcli) references clientes(idcli)
+    );
+    
+    -- mostrar a tabela
+    describe tbos;
+
+insert into clientes (nome,fone) values('Kelly Cristina','31256-2222');
+insert into clientes (nome,fone) values('José de Assis','91234-1111');
+
+select * from clientes;
+
+insert into tbOs(equipamento,defeito,status0s,idcli)
+values('Notebook Lenovo modelo','Não liga','Orçamento',1);
+
+
+
+insert into tbOs(equipamento,defeito,status0s,valor,idcli)
+values('PC Positivo','Formatação do Windows','Aprovado',80,2);
+
+
+
+select * from tbOs;
+
+
